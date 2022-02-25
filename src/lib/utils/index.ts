3df693dd5b40973deefa55cb447f4501aa4d6a34
Jon Diep <jondiep1@gmail.com>
2022-02-24 17:24:36 -0800

    minting example

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,3 +1,7 @@
+import { getAddress } from '@ethersproject/address';
+import { AddressZero } from '@ethersproject/constants';
+import { Contract } from '@ethersproject/contracts';
+import { JsonRpcSigner, Web3Provider } from '@ethersproject/providers';
 import _ from 'lodash';
 
 export * from './definitions';
@@ -9,3 +13,46 @@ export const camelize = (obj: any) =>
 
     acc[camelKey] = _.isObject(value) ? camelize(value) : value;
   });
+
+// returns the checksummed address if the address is valid, otherwise returns false
+export function isAddress(value: any): string | false {
+  try {
+    return getAddress(value);
+  } catch {
+    return false;
+  }
+}
+
+// account is not optional
+export function getSigner(
+  library: Web3Provider,
+  account: string,
+): JsonRpcSigner {
+  return library.getSigner(account).connectUnchecked();
+}
+
+// account is optional
+export function getProviderOrSigner(
+  library: Web3Provider,
+  account?: string,
+): Web3Provider | JsonRpcSigner {
+  return account ? getSigner(library, account) : library;
+}
+
+// account is optional
+export function getContract(
+  address: string,
+  ABI: any,
+  library: Web3Provider,
+  account?: string,
+): Contract {
+  if (!isAddress(address) || address === AddressZero) {
+    throw Error(`Invalid 'address' parameter '${address}'.`);
+  }
+
+  return new Contract(
+    address,
+    ABI,
+    getProviderOrSigner(library, account) as any,
+  );
+}
